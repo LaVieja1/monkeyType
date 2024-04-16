7e2f5cc0e907d582be55f2112f669f5bed06b137
Santi <canosantiago404@gmail.com>
2024-04-16 19:28:58 -0300

    variables iniciales

---
+++ script.js
@@ -0,0 +1,17 @@
+$time = document.querySelector("time");
+$paragraph = document.querySelector("p");
+$input = document.querySelector("input");
+
+const INITIAL_TIME = 30;
+
+const TEXT =
+  "the quick brown fox jumps over the lazy dog and the lazy dog jumps over the quick brown fox";
+
+let words = [];
+let currentTime = INITIAL_TIME;
+
+initGame();
+initEvents();
+
+function initGame() {}
+function initEvents() {}
